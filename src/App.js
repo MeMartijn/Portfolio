1fe1ec398f49d6c16440e36aa84e2bc581e2576b
Martijn <martijn-1998@hotmail.com>
2018-11-06 21:56:33 +0100

    chore: added particles and background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import './styles/reset.css';
+import './styles/main.css';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <Particles
+        params = {{
+          'particles': {
+              'number': {
+                  'value': 110
+              },
+              'size': {
+                  'value': 1
+              },
+              'move': {
+                'speed': 1,
+              },
+              'interactivity': {
+                'events': {
+                  'onhover': {
+                    'enable': true,
+                    'mode': 'repulse'
+                  }
+                }
+              }
+            }
+      }}
+
+      style = {{
+        'width': '10%',
+        'height': '100vw'
+      }}
+      />
     );
   }
 }
