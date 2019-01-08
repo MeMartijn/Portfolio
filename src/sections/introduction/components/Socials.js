355ea572eda1ace656134bda4304b4e51031f8ff
Martijn <martijn-1998@hotmail.com>
2019-01-08 10:35:57 +0100

    refactor: remove unused vars

--- src/sections/introduction/components/Socials.js
+++ src/sections/introduction/components/Socials.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
